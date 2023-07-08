bba2d43f2bd61dc6291a371e6944437860199617
juliusbaba <53842713+juliusbaba@users.noreply.github.com>
2023-07-08 18:08:27 -0400

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://3.94.132.147:9000',
+  serverUrl: 'http://3.94.132.147:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
